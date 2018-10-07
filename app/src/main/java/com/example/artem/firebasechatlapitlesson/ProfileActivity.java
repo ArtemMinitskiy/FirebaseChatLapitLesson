ec022a443f1c13970346b15daf1dd7ea02856f52
rukok <rukokuro@gmail.com>
2018-10-07 21:19:50 +0200

    ProfileActivity Part 17 - complete

--- app/src/main/java/com/example/artem/firebasechatlapitlesson/ProfileActivity.java
+++ app/src/main/java/com/example/artem/firebasechatlapitlesson/ProfileActivity.java
@@ -1,22 +1,117 @@
 package com.example.artem.firebasechatlapitlesson;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private TextView profileName;
+    private ImageView imageView;
+    private TextView profileName, profileStatus, profileFriendsCount;
+    private Button sendReqBtn;
+
+    private FirebaseUser firebaseAuth;
+
+    private String current_state;
+
+    private DatabaseReference usersReference, friendsReference;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        String user_id = getIntent().getStringExtra("user_id");
+        final String user_id = getIntent().getStringExtra("user_id");
 
+        firebaseAuth = FirebaseAuth.getInstance().getCurrentUser();
+        usersReference = FirebaseDatabase.getInstance().getReference().child("ChatUsers").child(user_id);
+        friendsReference = FirebaseDatabase.getInstance().getReference().child("Friend_request");
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Loading...");
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        current_state = "not_friends";
+
+        imageView = (ImageView) findViewById(R.id.profile_image);
         profileName = (TextView) findViewById(R.id.profile_name);
-        profileName.setText(user_id);
+        profileStatus = (TextView) findViewById(R.id.profile_status);
+        profileFriendsCount = (TextView) findViewById(R.id.profile_totalFriends);
+        sendReqBtn = (Button) findViewById(R.id.profile_send_req_btn);
+
+        usersReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                profileName.setText(name);
+                profileStatus.setText(status);
+
+                Picasso.get().load(image).placeholder(R.drawable.user_default).into(imageView);
+
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        sendReqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (current_state.equals("not_friends")){
+
+                    friendsReference.child(firebaseAuth.getUid())
+                            .child(user_id)
+                            .child("request_type")
+                            .setValue("sent")
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                friendsReference.child(user_id)
+                                        .child(firebaseAuth.getUid())
+                                        .child("request_type")
+                                        .setValue("received").addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(ProfileActivity.this, "Request Sent Successfully", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }else {
+                                Toast.makeText(ProfileActivity.this, "Failed Sending Request", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
     }
 }
