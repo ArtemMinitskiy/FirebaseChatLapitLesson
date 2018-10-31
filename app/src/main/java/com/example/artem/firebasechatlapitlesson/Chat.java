7dcf1bc30d899f722b4b50fd897352b1c6b85881
rukok <rukokuro@gmail.com>
2018-10-31 15:28:36 +0100

    Start new blog app, rework, update, and improve lessons.

--- app/src/main/java/com/example/artem/firebasechatlapitlesson/Chat.java
+++ app/src/main/java/com/example/artem/firebasechatlapitlesson/Chat.java
@@ -13,7 +13,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
-public class LapitChat extends Application {
+public class Chat extends Application {
 
     private DatabaseReference userDatabase;
     private FirebaseAuth userAuth;
